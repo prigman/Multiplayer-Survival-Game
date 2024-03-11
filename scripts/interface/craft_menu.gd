7a9c2c895d235f32e666193111ecbedf1490ab45
prigman <prigman48@gmail.com>
2024-03-11 01:26:34 +0100

    фикс багов (спс chatgpt)

--- scripts/interface/craft_menu.gd
+++ scripts/interface/craft_menu.gd
@@ -74,40 +74,55 @@ func _on_craft_button_pressed(slot_data : InSlotData):
 		print("player_inventory_slots: %s" % player_inventory_slots.size())
 		print("player_quick_slots: %s" % player_quick_slots.size())
 		var components_available : int = 0
+		var used_components := {}
 		for inv_slot_data in player_inventory_slots:
 			if inv_slot_data and inv_slot_data.item:
 				for craft_item in item_data.craft_components:
 					if craft_item and craft_item.component == inv_slot_data.item:
-						if inv_slot_data.amount_in_slot >= craft_item.amount:
+						var available = inv_slot_data.amount_in_slot - used_components.get(inv_slot_data.item, 0)
+						if available >= craft_item.amount:
 							print("able to craft with item: %s" % inv_slot_data.item.name)
 							components_available += 1
+							used_components[inv_slot_data.item] = used_components.get(inv_slot_data.item, 0) + craft_item.amount
 						else:
 							print("item available but not enough: %s" % inv_slot_data.item.name)
 		for quick_slot in player_quick_slots:
 			if quick_slot and quick_slot.item:
 				for craft_item in item_data.craft_components:
 					if craft_item and craft_item.component == quick_slot.item:
-						if quick_slot.amount_in_slot >= craft_item.amount:
+						var available = quick_slot.amount_in_slot - used_components.get(quick_slot.item, 0)
+						if available >= craft_item.amount:
 							print("from quick slots able to craft with item: %s" % quick_slot.item.name)
 							components_available += 1
+							used_components[quick_slot.item] = used_components.get(quick_slot.item, 0) + craft_item.amount
 						else:
 							print("from quick slots item available but not enough: %s" % quick_slot.item.name)
 		# достаточно ли компонентов для крафта
 		if components_available == item_data.craft_components.size():
-			for inv_slot_data in player_inventory_slots:
-				if inv_slot_data and inv_slot_data.item:
-					for craft_item in item_data.craft_components:
-						if craft_item and craft_item.component == inv_slot_data.item:
-							if inv_slot_data.amount_in_slot >= craft_item.amount:
-								inv_slot_data.amount_in_slot -= craft_item.amount
-			for quick_slot in player_quick_slots:
-				if quick_slot and quick_slot.item:
-					for craft_item in item_data.craft_components:
-						if craft_item and craft_item.component == quick_slot.item:
-							if quick_slot.amount_in_slot >= craft_item.amount:
-								quick_slot.amount_in_slot -= craft_item.amount
+			for craft_item in item_data.craft_components:
+				var amount_used = used_components.get(craft_item.component, 0)
+				# Обновление количества предметов в слотах
+				for i in range(player_inventory_slots.size()):
+					var inv_slot_data = player_inventory_slots[i]
+					if inv_slot_data and inv_slot_data.item == craft_item.component:
+						var available_in_slot = inv_slot_data.amount_in_slot
+						var used_from_slot = min(available_in_slot, amount_used)
+						inv_slot_data.amount_in_slot -= used_from_slot
+						if inv_slot_data.amount_in_slot == 0:
+							player_inventory_slots[i] = null  # Устанавливаем элемент массива в null
+						amount_used -= used_from_slot
+
+				for i in range(player_quick_slots.size()):
+					var quick_slot = player_quick_slots[i]
+					if quick_slot and quick_slot.item == craft_item.component:
+						var available_in_slot = quick_slot.amount_in_slot
+						var used_from_slot = min(available_in_slot, amount_used)
+						quick_slot.amount_in_slot -= used_from_slot
+						if quick_slot.amount_in_slot == 0:
+							player_quick_slots[i] = null  # Устанавливаем элемент массива в null
+						amount_used -= used_from_slot
 			Global.global_player_inventory.signal_inventory_update.emit(Global.global_player_inventory)
 			Global.global_player_quick_slot.signal_inventory_update.emit(Global.global_player_quick_slot)
-			if !Global.global_player_inventory._pick_up_slot_data(slot_data):
-				if !Global.global_player_quick_slot._pick_up_slot_data(slot_data):
-					Global.global_player.inventory_interface.signal_drop_item.emit(slot_data)
+			if !Global.global_player_inventory._pick_up_slot_data(slot_data.duplicate()):
+				if !Global.global_player_quick_slot._pick_up_slot_data(slot_data.duplicate()):
+					Global.global_player.inventory_interface.signal_drop_item.emit(slot_data.duplicate())
